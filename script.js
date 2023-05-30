f0786697006e144ff6952f8d4ed7f1e390e0698c
vickyyylopez <victoriaagostinalopez@gmail.com>
2023-05-30 17:58:06 -0300

    Entrega 3

---
+++ script.js
@@ -0,0 +1,23 @@
+// Array de productos
+var productos = [
+    {
+      nombre: 'Llaveros Souvenirs + Tag Envoltorios + Estampitas',
+      precio: 4250,
+    },
+    {
+      nombre: 'Llaveros Souvenirs + Estuches Envoltorios + Estampitas',
+      precio: 4000,
+    },
+    {
+      nombre: 'Llaveros Souvenirs + Estampitas',
+      precio: 3700,
+    }
+  ];
+  
+  // Función para añadir un producto al carrito
+  function sumar(precio, nombre) {
+    // Lógica para añadir el producto al carrito
+    alert ("Aun no se puede acceder al carrito")
+  }
+  
+
